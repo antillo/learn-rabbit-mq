90afaa2a3e07d9c1b11b1514d044267d195916f4
ANTONIO JESUS <antonio.jesus.sanchez.colmenero@everis.com>
2021-02-26 15:09:31 +0100

    inicio

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace learn_rabbit_mq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        }
+    }
+}
